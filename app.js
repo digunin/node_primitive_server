77f6d49b2b777906ba1aefee6aeaf75228ab4f43
Dmitry Gunin <kandidad@gmail.com>
2021-01-19 23:40:53 +0300

    Простая маршрутизация, +установка header(чтобы браузер обрабатывал теги html)

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const http = require('http');
 const PORT = 3000;
 
 http.createServer((req, res)=>{
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    switch(req.url){
+        case "/":
+            res.end("<h1>Home page</h1>");
+            break;
+        case "/hello":
+            res.end("<h1>Hello!</h1>");
+            break;
+    }
     res.end("Hello!");
 }).listen(PORT, ()=>console.log(`Server started at port${PORT}`));
